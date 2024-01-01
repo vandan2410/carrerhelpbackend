f30d199c32f52ab5e89bf6c2ed99308c813b37b8
Avi <avishkar.yt@gmail.com>
2024-01-01 07:46:06 +0530

    updated logout route and deleted old server file

--- controller/user.js
+++ controller/user.js
@@ -61,12 +61,8 @@ export const loginUser = async (req, res) => {
 };
 
 export const logoutUser = async (req, res) => {
-  // Set token to none and expire after 5 seconds
-  res.cookie('bigCookie', 'none', {
-      expires: new Date(Date.now() + 5 * 1000),
-      httpOnly: true,
-  })
+  res.clearCookie("bigCookie");
   res
-      .status(200)
-      .json({ success: true, message: 'User logged out successfully' })
-}
+    .status(200)
+    .json({ success: true, message: "User logged out successfully" });
+};
